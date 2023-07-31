dae856ad3d39083b03c6a74910580b03ca2f3fd6
marisasaunders <marisaksaunders@gmail.com>
2023-07-31 12:28:35 -0400

    start to build setup component

--- client/src/components/Setups.js
+++ client/src/components/Setups.js
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
+import { Route, useParams } from "react-router-dom";
+
+// these are all components nancy used, not sure if I will use them too, just put them here as an option to consider
+// import SetupForm from "./SetupForm"
+// import SetupLinks from "./SetupLinks"
+// import Setup from './Setup'
 
 function Setups() {
   return (
