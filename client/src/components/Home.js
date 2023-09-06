47929e3ab04511bbfb9c9d8f752ad3ac9fc3fa96
marisasaunders <marisaksaunders@gmail.com>
2023-09-06 13:53:51 -0400

    write up readme

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -28,7 +28,7 @@ function Home() {
         </header>
         <main role="main" class="inner cover">
           <h1 class="cover-heading">Welcome!</h1>
-          <p class="lead">BleepBloop is a website for electronic musicians to organize their dawless music setups. Rather than trying to remember different systems for various performances or songs, BleepBloop is a place where you can create and organize your creations in one space. Please signup or login above. For more information on how this application was create please click below. </p>
+          <p class="lead">BleepBloop is a website for electronic musicians to organize their dawless music setups. Rather than trying to remember different systems for various performances or songs, BleepBloop is a place where you can create and organize your creations in one space. Please signup or login above. For more information on how this application was created please click below. </p>
           <p class="lead">
             <a href="https://github.com/marskimiko/bleepbloop-application" class="btn btn-lg btn-secondary">Learn more</a>
           </p>
